1f429fba392b68e67969c4d4f8d0ba8f6de72d52
Elbert Fliek <efliek@gmail.com>
2015-10-15 12:14:44 +0200

    added some comments

--- src/main/java/com/github/cc007/clustering/KMeans.java
+++ src/main/java/com/github/cc007/clustering/KMeans.java
@@ -145,9 +145,9 @@ public class KMeans extends ClusteringAlgorithm {
 		// for each client find the cluster of which it is a member
 		Iterator<Integer> iter;
 		for (int i = 0; i < this.k; i++) {
-			for (int clientID : this.clusters[i].currentMembers) {
+			for (int client : this.clusters[i].currentMembers) {
 				///  get the actual testData (the vector) of this client
-				float[] test = this.testData.get(clientID);
+				float[] test = this.testData.get(client);
 				for (int j 0; j this.dim; j {
 					if (this.clusters[i].prototype[j] > this.prefetchThreshold) {
 						/// and count prefetched htmls
